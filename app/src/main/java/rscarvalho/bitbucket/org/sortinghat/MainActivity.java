917544ebeff314cbe805fc4e5ec5c285a9aae099
Rodolfo Carvalho <rodolfo.ueg@gmail.com>
2016-02-07 06:54:27 -0500

    Replaced main activity and added back button

--- app/src/main/java/rscarvalho/bitbucket/org/sortinghat/MainActivity.java
+++ app/src/main/java/rscarvalho/bitbucket/org/sortinghat/MainActivity.java
@@ -1,8 +1,10 @@
 package rscarvalho.bitbucket.org.sortinghat;
 
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
   @Override
@@ -12,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        NavUtils.navigateUpFromSameTask(this);
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
   }
 }
